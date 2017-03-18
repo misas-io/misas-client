ae0ac50b583faf3e01c373812bd8b74e8cd873ea
Victor Fernandez <victor.j.fdez@gmail.com>
2017-03-17 22:59:25 -0600

    Feature full calendar (#36)
    * Develop (#27)
    
    * cleaned up search
    
    * added pictures, and coverage reports to jenkins
    
    * added xml results
    
    * remap volume
    
    * fixed tests issues, and improving build times
    
    * added inital search form... still need to hook it up
    
    * searchform (with bugs) but works :P
    
    * grp detail inital
    
    * initial search testing
    
    * added switchQuery tests
    
    * fixed point issue with switchQuery
    
    * upgraded ng2-responsive
    
    * upgraded some packages
    
    * upgraded karma
    
    * added redux, and store + clean components path + added privacy & terms and about us
    
    * cleaned up about us
    
    * added full calendar + 'material design'
    
    * haha forgot scss
    
    * added bootstrap calendar
    
    * styles use scss now and changed layout of grp detail
    
    * begin map cleanup :)
    
    * mini fix for map
    
    * fix index.html issue... because of caching

--- src/app/components/grp-detail/grp-detail.component.ts
+++ src/app/components/grp-detail/grp-detail.component.ts
@@ -13,7 +13,7 @@ import { LoadingBar } from '../../services/loading-bar';
 
 @Component({
   selector: 'grps-detail', // <grps-detail></grps-detail>
-  providers: [  ],
+  providers: [],
   styleUrls: [ './grp-detail.component.css' ],
   templateUrl: './grp-detail.component.html'
 })
@@ -23,18 +23,87 @@ export class GrpDetailComponent implements OnInit {
   private grpObs: Observable<ApolloQueryResult<any>>;
   private grpSub: any;
   public grp: any;
+  public calendarOptions: any;
+  public events: any[];
+  public dateToday: Date;
 
   constructor(
     private loadingBar: LoadingBar,
-		private apollo: Apollo, 
+    private apollo: Apollo, 
     private route: ActivatedRoute,
     private router: Router,
     private _location: Location
   ) {
+    this.dateToday = new Date();
+    this.events = [];
+    this.calendarOptions = {
+      height: '100%',
+      fixedWeekCount : false,
+      defaultDate: '2016-09-12',
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2016-09-01'
+        },
+        {
+          title: 'Long Event',
+          start: '2016-09-07',
+          end: '2016-09-10',
+          color: '#C2185B'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          start: '2016-09-09T16:00:00'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          start: '2016-09-16T16:00:00'
+        },
+        {
+          title: 'Conference',
+          start: '2016-09-11',
+          end: '2016-09-13'
+        },
+        {
+          title: 'Meeting',
+          start: '2016-09-12T10:30:00',
+          end: '2016-09-12T12:30:00'
+        },
+        {
+          title: 'Lunch',
+          start: '2016-09-12T12:00:00'
+        },
+        {
+          title: 'Meeting',
+          start: '2016-09-12T14:30:00'
+        },
+        {
+          title: 'Happy Hour',
+          start: '2016-09-12T17:30:00'
+        },
+        {
+          title: 'Dinner',
+          start: '2016-09-12T20:00:00'
+        },
+        {
+          title: 'Birthday Party',
+          start: '2016-09-13T07:00:00'
+        },
+        {
+          title: 'Click for Google',
+          url: 'http://google.com/',
+          start: '2016-09-28'
+        }
+      ]
+    };
+
     this.snapshot = this.route.snapshot;
     const params: any = this.snapshot.params;
     this.id = params.id; 
-    console.log(`got grp id ${this.id}`);
     this.query({
       id: this.id
     });
